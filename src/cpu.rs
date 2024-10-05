6fda4a7bf65248aa1b81cc478235d848673182b5
frazier_g <garyfrazier95@yahoo.com>
2024-10-05 18:48:38 -0400

    stub out rest of instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -246,3 +246,134 @@ pub fn snexy(emulator: &mut Emulator, instruction: u16) {
         emulator.pc += 2;
     }
 }
+
+// DELETE
+// _ if (instruction & 0xA000 == 0xA000) => ldi(emulator, instruction), // Annn
+// _ if (instruction & 0xB000 == 0xB000) => jpv(emulator, instruction), // Bnnn
+// _ if (instruction & 0xC000 == 0xC000) => rnd(emulator, instruction), // Cxkk
+// _ if (instruction & 0xD000 == 0xD000) => drw(emulator, instruction), // Dxyn
+// _ if (instruction & 0xE09E == 0xE09E) => skp(emulator, instruction), // Ex9E
+// _ if (instruction & 0xE0A1 == 0xE0A1) => sknp(emulator, instruction), // ExA1
+// _ if (instruction & 0xF007 == 0xF007) => ldxdt(emulator, instruction), // Fx07
+// _ if (instruction & 0xF00A == 0xF00A) => ldk(emulator, instruction), // Fx0A
+// _ if (instruction & 0xF015 == 0xF015) => lddt(emulator, instruction), // Fx15
+// _ if (instruction & 0xF018 == 0xF018) => ldst(emulator, instruction), // Fx18
+// _ if (instruction & 0xF01E == 0xF01E) => addi(emulator, instruction), // Fx1E
+// _ if (instruction & 0xF029 == 0xF029) => ldiv(emulator, instruction), // Fx29
+// _ if (instruction & 0xF033 == 0xF033) => ldb(emulator, instruction), // Fx33
+// _ if (instruction & 0xF055 == 0xF055) => ldii(emulator, instruction), // Fx55
+// _ if (instruction & 0xF065 == 0xF065) => ldvi(emulator, instruction), // Fx65
+
+// Annn - LD I, addr
+// Set I = nnn.
+// The value of register I is set to nnn.
+pub fn ldi(emulator: &mut Emulator, instruction: u16) {
+    
+}
+
+// Bnnn - JP V0, addr
+// Jump to location nnn + V0.
+// The program counter is set to nnn plus the value of V0.
+pub fn jpv(emulator: &mut Emulator, instruction: u16) {
+    
+}
+
+// Cxkk - RND Vx, byte
+// Set Vx = random byte AND kk.
+// The interpreter generates a random number from 0 to 255, which is then ANDed with the value kk. 
+// The results are stored in Vx. See instruction 8xy2 for more information on AND.
+pub fn rnd(emulator: &mut Emulator, instruction: u16) {
+    
+}
+
+// Dxyn - DRW Vx, Vy, nibbler
+// Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
+/* 
+he interpreter reads n bytes from memory, starting at the address stored in I. 
+These bytes are then displayed as sprites on screen at coordinates (Vx, Vy). 
+Sprites are XORed onto the existing screen. If this causes any pixels to be erased, VF is set to 1, otherwise it is set to 0. 
+If the sprite is positioned so part of it is outside the coordinates of the display, it wraps around to the opposite side of the screen.
+ See instruction 8xy3 for more information on XOR, and section 2.4, Display, for more information on the Chip-8 screen and sprites.
+*/
+pub fn drw(emulator: &mut Emulator, instruction: u16) {
+    
+}
+
+// Ex9E - SKP Vx
+// Skip next instruction if key with the value of Vx is pressed.
+// Checks the keyboard, and if the key corresponding to the value of Vx is currently in the down position, PC is increased by 2.
+pub fn skp(emulator: &mut Emulator, instruction: u16) {
+    
+}
+
+// ExA1 - SKNP Vx
+// Skip next instruction if key with the value of Vx is not pressed.
+// Checks the keyboard, and if the key corresponding to the value of Vx is currently in the up position, PC is increased by 2.
+pub fn sknp(emulator: &mut Emulator, instruction: u16) {
+    
+}
+
+// Fx07 - LD Vx, DT
+// Set Vx = delay timer value.
+// The value of DT is placed into Vx.
+pub fn ldxdt(emulator: &mut Emulator, instruction: u16) {
+    
+}
+
+// Fx0A - LD Vx, K
+// Wait for a key press, store the value of the key in Vx.
+// All execution stops until a key is pressed, then the value of that key is stored in Vx.
+pub fn ldk(emulator: &mut Emulator, instruction: u16) {
+    
+}
+
+// Fx15 - LD DT, Vx
+// Set delay timer = Vx.
+// DT is set equal to the value of Vx.
+pub fn lddt(emulator: &mut Emulator, instruction: u16) {
+    
+}
+
+// Fx18 - LD ST, Vx
+// Set sound timer = Vx.
+// ST is set equal to the value of Vx.
+pub fn ldst(emulator: &mut Emulator, instruction: u16) {
+    
+}
+
+// Fx1E - ADD I, Vx
+// Set I = I + Vx.
+// The values of I and Vx are added, and the results are stored in I.
+pub fn addi(emulator: &mut Emulator, instruction: u16) {
+    
+}
+
+// Fx29 - LD F, Vx
+// Set I = location of sprite for digit Vx.
+// The value of I is set to the location for the hexadecimal sprite corresponding to the value of Vx. 
+// See section 2.4, Display, for more information on the Chip-8 hexadecimal font.
+pub fn ldiv(emulator: &mut Emulator, instruction: u16) {
+    
+}
+
+// Fx33 - LD B, Vx
+// Store BCD representation of Vx in memory locations I, I+1, and I+2.
+// The interpreter takes the decimal value of Vx, and places the hundreds digit in memory at location in I,
+// the tens digit at location I+1, and the ones digit at location I+2.
+pub fn ldb(emulator: &mut Emulator, instruction: u16) {
+    
+}
+
+// Fx55 - LD [I], Vx
+// Store registers V0 through Vx in memory starting at location I.
+// The interpreter copies the values of registers V0 through Vx into memory, starting at the address in I.
+pub fn ldii(emulator: &mut Emulator, instruction: u16) {
+    
+}
+
+// Fx65 - LD Vx, [I]
+// Read registers V0 through Vx from memory starting at location I.
+// The interpreter reads values from memory starting at location I into registers V0 through Vx.
+pub fn ldvi(emulator: &mut Emulator, instruction: u16) {
+    
+}
